53607d2974bbee8486b6085fa72a6668747edf0a
Wood Mutran <49340751+woodmutran@users.noreply.github.com>
2022-08-29 16:13:27 -0300

    Delete lp-dsc01-a directory

--- lp-dsc01-a/js/typewriting.js
+++
@@ -1,9 +0,0 @@
-var typeWriting = new TypeWriting({
-    targetElement   : document.getElementsByClassName('terminal')[0],
-    inputString     : 'DATA SCIENCE DEGREE',
-    typing_interval : 85, // Interval between each character
-    blink_interval  : '0.9s', // Interval of the cursor blinks
-    cursor_color    : '#fff', // Color of the cursor
-    }, function() {
-    console.log("END");
-});
